8d01d413b26a57e331349b6860673479edc9754b
chimdice <118031439+chimdice@users.noreply.github.com>
2024-07-03 15:50:39 -0700

    Render the cv element on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function CV (props) {
   return (
     <div className="cv">
-      <h2>{props.field}</h2>
+      <h2>{props.field}: </h2>
       <h3>{props.info}</h3>
     </div>
   )
@@ -15,12 +15,10 @@ function CV (props) {
 function FormElement (props) {
 
   return (
-    <div className="form-element">
+    <div className="form-element" style={{"padding":"10px"}}>
       <label htmlFor={props.field}>{props.field}: </label>
       <div>
         <input type={props.type} id={props.field} value={props.value} onChange={(e) => props.onType(e.target.value)}/>
-      </div>
-      <div>
         <button onClick={(e) => {
           e.preventDefault();
           const input = document.querySelector("#"+props.field);
@@ -37,7 +35,10 @@ function App() {
   const [cvName, updateCVName] = useState("");
 
   const [emailInfo, updateEmailInfo] = useState("");
+  const [cvEmail, updateCVEmail] = useState("");
+
   const [phoneInfo, updatePhoneInfo] = useState("");
+  const [cvPhone, updateCVPhone] = useState("");
 
   return (
     <>
@@ -45,15 +46,20 @@ function App() {
         <h1>CV Creator</h1>
       </header>
       <main>
+      <div className="current-cv">
+          <h2>Current CV</h2>
+            <CV field="name" info={cvName}/>
+            <CV field="email" info={cvEmail}/>
+            <CV field="phone number" info={cvPhone}/>
+        </div>
         <div className="form">
           <h2>Please Enter each field</h2>
           <form>
             <FormElement type="text" field="name" value={nameInfo} onClick={updateCVName} onType={updateNameInfo}/>
-            <FormElement type="email" field="email" value={emailInfo} onClick={updateEmailInfo} onType={updateEmailInfo}/>
-            <FormElement type="tel" field="phone number" value={phoneInfo} onClick={updatePhoneInfo} onType={updateEmailInfo}/>
+            <FormElement type="email" field="email" value={emailInfo} onClick={updateCVEmail} onType={updateEmailInfo}/>
+            <FormElement type="tel" field="phone-number" value={phoneInfo} onClick={updateCVPhone} onType={updatePhoneInfo}/>
           </form>
         </div>
-        <CV field="name" info={cvName}/>
       </main>
     </>
   )
